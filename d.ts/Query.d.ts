cd0ba85c74b7103bae0287ea63136fb487ba7d42
Christian Schwerdt <christian@cmft.io>
2018-10-23 13:37:38 +0200

    change query type from 'any' to 'QueryOptions'

--- d.ts/Query.d.ts
+++ d.ts/Query.d.ts
@@ -3,12 +3,12 @@ import { QueryOptions } from './QueryOptions';
 export default class Query {
     protected repository: string | undefined;
     protected endpoint: string | undefined;
-    protected query: any;
+    protected query: QueryOptions;
     protected entityId: string | undefined;
     protected httpClient: HttpClient;
     constructor(endpoint: string, repository: string, httpClient: HttpClient, queryOptions?: QueryOptions, entityId?: string);
     setQuery(queryOptions: QueryOptions): void;
-    getQuery(): any;
+    getQuery(): QueryOptions;
     execute(): Promise<any>;
     getEndpoint(entityId?: string): string;
 }
